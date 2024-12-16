b21ffaee2dd7c32e2107ecf4ac1b02441bb3fb42
malbornl <malbornl@student.21-school.ru>
2024-12-17 00:30:58 +0300

    filter

--- Cytology/main.py
+++ Cytology/main.py
@@ -1,27 +1,51 @@
 import cv2
 import numpy as np
 import imutils
+import os
 
 from matplotlib import pyplot as pl
 
-#path  = '/home/pashka/Documents/GitHub/Stepik_opencv/nobrain'
 path = '/home/gans/Документы/Github/Neuropass_CS/Cytology'
+temp = path + '/temp/'
+temp_images = os.listdir(temp)
+i = 1
+
+for temp_image in temp_images:
+    os.remove(temp + temp_image)
+
+def export_temp_img(image, name, num):
+    cv2.imwrite(path + '/temp/' + num + '_' + name, image)
+   
 
 img1 = cv2.imread(path  + '/img/1.jpg')
-#img1 = cv2.resize(img1, (img1.shape[1] // 2 ,img1.shape[0] // 2))
-
 img2 = cv2.imread(path  + '/img/2.jpg')
 img3 = cv2.imread(path  + '/img/3.jpg')
 #img3 = img3[img3.shape[1] // 3 : img3.shape[1], 0 : img3.shape[0] // 2]
 img4 = cv2.imread(path  + '/img/4.jpg')
 
 img = img3
+export_temp_img(img, 'current.jpg', str(i))
+i = i + 1
+
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+lower_red = np.array([170,50,50])
+upper_red = np.array([180,255,255])
+mask1 = cv2.inRange(hsv, lower_red, upper_red)
+target = cv2.bitwise_and(img,img, mask=mask1)
+
+cv2.imshow('target', target)
+cv2.waitKey(0)
 
 #в оттенки серого
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+export_temp_img(gray, 'gray.jpg', str(i))
+i = i + 1
+
 
 #cнижение шумов
 img_filter = cv2.bilateralFilter(gray, 11, 15, 15)
+export_temp_img(img_filter, 'filter.jpg', str(i))
+i = i + 1
 
 """
 cv2.bilateralFilter(src, d, sigma_color, sigma_space)
@@ -34,6 +58,8 @@ cv2.bilateralFilter(src, d, sigma_color, sigma_space)
 
 #определение контуров
 edges = cv2.Canny(img_filter, 5, 150)
+export_temp_img(edges, 'edges.jpg', str(i))
+i = i + 1
 
 """
 cv2.Canny(image, threshold1, threshold2[, edges[, apertureSize[, L2gradient]]])
@@ -46,39 +72,18 @@ cv2.Canny(image, threshold1, threshold2[, edges[, apertureSize[, L2gradient]]])
 """
 
 
+
+
 """
+#вывод на экран этапов обработки изображений
+temp_images = os.listdir(temp)
+temp_images.sort()
+print(temp_images)
+
+for temp_image in temp_images:
+    i = cv2.imread(temp + temp_image)
+    cv2.imshow(temp_image, i)
+    cv2.waitKey(0)
 
 
-cont = cv2.findContours(edges.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-cont = imutils.grab_contours(cont)
-cont = sorted(cont, key=cv2.contourArea, reverse=True)[:8]
-
-pos = None
-for i in cont:
-    approx = cv2.approxPolyDP(i, 10, True)
-    if len(approx) == 4:
-        pos = approx
-        break
-
-mask = np.zeros(gray.shape, np.uint8)
-new_img = cv2.drawContours(mask, [pos], 0, 255, -1)
-bitwise_img = cv2.bitwise_and(img, img, mask=mask)
-
-(x, y) = np.where(mask==255)
-(x1, y1) = np.min(x), np.min(y)
-(x2, y2) = np.max(x), np.max(y)
-crop = gray[x1:x2, y1:y2]
-
-
-#pl.imshow(cv2.cvtColor(crop, cv2.COLOR_BGR2RGB))
-#pl.show()
 """
-
-cv2.imshow('res', img)
-cv2.waitKey(0)
-cv2.imshow('res', gray)
-cv2.waitKey(0)
-cv2.imshow('res', img_filter)
-cv2.waitKey(0)
-cv2.imshow('res', edges)
-cv2.waitKey(0)
